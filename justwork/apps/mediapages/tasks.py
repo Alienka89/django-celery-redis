faf5bc7577d479bb2615ce4987ebce99ff5bd7a6
Alena Bulatova <alien89yar@gmail.com>
2018-08-28 09:11:55 +0300

    tests

--- justwork/apps/mediapages/tasks.py
+++ justwork/apps/mediapages/tasks.py
@@ -1,8 +1,3 @@
-import logging
-
-from django.urls import reverse
-from django.core.mail import send_mail
-from django.contrib.auth import get_user_model
 from justwork.celery import app
 from .models import Page,Block
 
--- justwork/apps/mediapages/tests.py
+++ justwork/apps/mediapages/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.urls.base import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import (Page, Block)
+import random
 
-# Create your tests here.
+
+class LittleTestClass(APITestCase,TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def _create_block(self, page_id):
+        rand = random.randint(1, 100)
+        Block.objects.create(
+            page_id=page_id,
+            title='Test title' + str(rand),
+            order_number=random.randint(1, 100),
+            text='Test text',
+            audio='course-audio.ogg',
+            bitrate='300',
+            video='https://www.youtube.com/watch?v=XOpDIGgePCM',
+            video_sub=''
+        )
+
+    def _create_page(self):
+        rand = random.randint(1, 100)
+        page = Page.objects.create(
+            order_number=rand,
+            title="Test title " + str(rand)
+        )
+        for i in range(5):
+            self._create_block(page.id)
+        return page
+
+    def test_create_page(self):
+        page = self._create_page()
+        self.assertTrue(len(page.title) > 0)
+        self.assertEqual(len(page.block.values('id')), 5)
+
+    def test_get_page(self):
+        page = self._create_page()
+        urls = ['http://127.0.0.1:8000/api/mediapages/',
+               'http://127.0.0.1:8000/api/mediapages/'+str(page.id)+'/']
+
+        for url in urls:
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
